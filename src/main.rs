5861a21938699f83b8bcd826c989717a28f8ccd6
RANDRIANAINA Georges Aaron <georges-aaron.randrianaina@irisa.fr>
2022-04-25 18:56:12 +0200

    src/lib: main() correct errors, flush stdout(), rm unused packages, ...

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,53 @@
 use lmutib;
-use reqwest;
-use std::fs::{File, self};
-use std::io::copy;
-use std::env;
+use std::fs::{self};
+use std::io::{self, Write};
 use std::path::Path;
-use git2::{Index, IndexAddOption, Repository, Oid, Error, Config};
 
 fn main() {
 
-    let kernel = "";
+    let kernel = "/home/linux-5.13";
     println!("┌───────────────────────────┐");
+    io::stdout().flush().unwrap();
     println!("· Experiment initialization ·");
+    io::stdout().flush().unwrap();
     println!("└───────────────────────────┘");
-
+    io::stdout().flush().unwrap();
     println!("  → Kernel directory: {}", kernel);
+    io::stdout().flush().unwrap();
     fs::remove_file([kernel, ".gitignore"].join("/"));
     print!("  → Initializing git directory...");
+    io::stdout().flush().unwrap();
     let git = lmutib::MyGit::new(kernel);
     println!(" ✓");
-
+    io::stdout().flush().unwrap();
     print!("  → Local git configuration...");
-    git.config("Tux", "None");
+    io::stdout().flush().unwrap();
+    let _ = git.config("Tux", "None");
     println!(" ✓");
-
+    io::stdout().flush().unwrap();
     print!("  → Adding source...");
+    io::stdout().flush().unwrap();
     let add1 = match git.add_all() {
         Ok (oid)  => oid,
         Err(err)  => panic!("\t/!\\ Error adding all.\n\t{:?}", err),
     };
     println!(" ✓");
-
+    io::stdout().flush().unwrap();
     print!("  → Committing sources...");
+    io::stdout().flush().unwrap();
     let src_commit = match git.commit("source", add1) {
         Ok (oid) => oid,
         Err(err) => panic!("\t/!\\ Error commit.\n\t{:?}", err),
     };
     println!(" ✓");
-
+    io::stdout().flush().unwrap();
     println!("┌───────────────────────────┐");
+    io::stdout().flush().unwrap();
     println!("·   Starting build tasks    ·");
+    io::stdout().flush().unwrap();
     println!("└───────────────────────────┘");
-
-    let data_configs = Path::new("/tmp/data-configs");
+    io::stdout().flush().unwrap();
+    let data_configs = Path::new("/home/data-configs");
 
     for folder in fs::read_dir(data_configs).unwrap().filter(|f| f.is_ok())
         .map(|f| f.unwrap()) {
@@ -52,35 +58,49 @@ fn main() {
             let base_config_branch = [&dir_name, "base", "cb"]
                 .join("-");
 
+            // CLEAN BUILD OF THE BASE CONFIGURATION
+            // -------------------------------------
+
             println!("  •  Folder: {}", &dir_name);
+            io::stdout().flush().unwrap();
             println!("  └─ Base configuration: {}", base_config_path);
+            io::stdout().flush().unwrap();
             print!("      → Creating new branch {}...", base_config_branch);
+            io::stdout().flush().unwrap();
             git.create_branch(&base_config_branch, src_commit);
             println!(" ✓");
+            io::stdout().flush().unwrap();
             print!("      → Checkout to {}...", base_config_branch);
+            io::stdout().flush().unwrap();
             git.checkout(&base_config_branch);
+            println!(" ✓");
+            io::stdout().flush().unwrap();
             print!("      → Copying configuration...", );
+            io::stdout().flush().unwrap();
             match fs::copy(base_config_path, [kernel, ".config"].join("/")) {
                 Ok (_) => println!(" ✓"),
                 Err(_) => panic!(" x"),
             };
-            println!(" ✓");
+            io::stdout().flush().unwrap();
             print!("      → Clean build...");
+            io::stdout().flush().unwrap();
             match lmutib::build(kernel) {
                 Ok (_) => {
                     print!(" ✓");
                     println!(" {}s", fs::read_to_string([kernel, "t+time"]
                                                         .join("/"))
-                             .unwrap());
+                             .unwrap().trim());
                 },
                 Err(_) => {
                     println!(" x");
                     println!("      ‗‗Trace‗‗\n      {:?}",
                              fs::read_to_string([kernel, "t+time"]
-                                                .join("/")).unwrap());
+                                                .join("/")).unwrap().trim());
                 }
             };
+            io::stdout().flush().unwrap();
             print!("      → Adding all...");
+            io::stdout().flush().unwrap();
             let addbase = match git.add_all() {
                 Ok (oid)  => {
                     println!(" ✓");
@@ -88,8 +108,9 @@ fn main() {
                 },
                 Err(err)  => panic!(" x\n\t{:?}", err),
             };
-
+            io::stdout().flush().unwrap();
             print!("      → Committing...");
+            io::stdout().flush().unwrap();
             let base_cb_commit = match git.commit("clean build", addbase) {
                 Ok (oid) => {
                     println!(" ✓");
@@ -97,6 +118,10 @@ fn main() {
                 },
                 Err(err) => panic!(" x\n\t{:?}", err),
             };
+            io::stdout().flush().unwrap();
+
+            // BUILDS OF THE MUTANTS
+            // ---------------------
 
             for config in fs::read_dir(folder.path()).unwrap().filter(|f| f.is_ok())
                 .map(|f| f.unwrap()){
@@ -107,34 +132,47 @@ fn main() {
                         let config_branch = [&dir_name, &file_name, "cb"]
                             .join("-");
 
+
+                        // CLEAN BUILD
+                        // ------------
+
                         println!("    └─ Considering {}", file_name);
+                        io::stdout().flush().unwrap();
                         print!("          → Creating new branch {}...", config_branch);
+                        io::stdout().flush().unwrap();
                         git.create_branch(&config_branch, src_commit);
                         println!(" ✓");
+                        io::stdout().flush().unwrap();
                         print!("          → Checkout to {}...", config_branch);
+                        io::stdout().flush().unwrap();
                         git.checkout(&config_branch);
-                        print!("          → Copying configuration...", );
+                        println!(" ✓");
+                        print!("          → Copying configuration...");
+                        io::stdout().flush().unwrap();
                         match fs::copy(&config_path, [kernel, ".config"].join("/")) {
                             Ok (_) => println!(" ✓"),
                             Err(_) => panic!(" x"),
                         };
-                        println!(" ✓");
+                        io::stdout().flush().unwrap();
                         print!("          → Clean build...");
+                        io::stdout().flush().unwrap();
                         match lmutib::build(kernel) {
                             Ok (_) => {
                                 print!(" ✓");
                                 println!(" {}s", fs::read_to_string([kernel, "t+time"]
                                                                     .join("/"))
-                                         .unwrap());
+                                         .unwrap().trim());
                             },
                             Err(_) => {
                                 println!(" x");
                                 println!("      ‗‗Trace‗‗\n      {:?}",
                                          fs::read_to_string([kernel, "t+time"]
-                                                            .join("/")).unwrap());
+                                                            .join("/")).unwrap().trim());
                             }
                         };
+                        io::stdout().flush().unwrap();
                         print!("          → Adding all...");
+                        io::stdout().flush().unwrap();
                         let addcb = match git.add_all() {
                             Ok (oid)  => {
                                 println!(" ✓");
@@ -144,7 +182,8 @@ fn main() {
                         };
 
                         print!("          → Committing...");
-                        let cb_commit = match git.commit("clean build", addcb) {
+                        io::stdout().flush().unwrap();
+                        let _cb_commit = match git.commit("clean build", addcb) {
                             Ok (oid) => {
                                 println!(" ✓");
                                 oid
@@ -155,34 +194,45 @@ fn main() {
                         let config_branch_ib = [&dir_name, &file_name, "ib"]
                             .join("-");
 
+                        // INCREMENTAL BUILD
+                        // -----------------
+
                         print!("          → Creating new branch {}...", config_branch_ib);
-                        git.create_branch(&config_branch_ib, cb_commit);
+                        io::stdout().flush().unwrap();
+                        git.create_branch(&config_branch_ib, base_cb_commit);
                         println!(" ✓");
-                        print!("          → Checkout to {}...", config_branch);
+                        io::stdout().flush().unwrap();
+                        print!("          → Checkout to {}...", config_branch_ib);
+                        io::stdout().flush().unwrap();
                         git.checkout(&config_branch_ib);
+                        println!(" ✓");
                         print!("          → Copying configuration...", );
+                        io::stdout().flush().unwrap();
                         match fs::copy(&config_path, [kernel, ".config"].join("/")) {
                             Ok (_) => println!(" ✓"),
                             Err(_) => panic!(" x"),
                         };
-                        println!(" ✓");
+                        io::stdout().flush().unwrap();
                         print!("          → Incremental build ({} → {})...",
                                base_config_branch, config_branch_ib);
+                        io::stdout().flush().unwrap();
                         match lmutib::build(kernel) {
                             Ok (_) => {
                                 print!(" ✓");
                                 println!(" {}s", fs::read_to_string([kernel, "t+time"]
                                                                     .join("/"))
-                                         .unwrap());
+                                         .unwrap().trim());
                             },
                             Err(_) => {
                                 println!(" x");
                                 println!("      ‗‗Trace‗‗\n      {:?}",
                                          fs::read_to_string([kernel, "t+time"]
-                                                            .join("/")).unwrap());
+                                                            .join("/")).unwrap().trim());
                             }
                         };
+                        io::stdout().flush().unwrap();
                         print!("          → Adding all...");
+                        io::stdout().flush().unwrap();
                         let addib = match git.add_all() {
                             Ok (oid)  => {
                                 println!(" ✓");
@@ -190,15 +240,17 @@ fn main() {
                             },
                             Err(err)  => panic!(" x\n\t{:?}", err),
                         };
-
+                        io::stdout().flush().unwrap();
                         print!("          → Committing...");
-                        let ib_commit = match git.commit("clean build", addib) {
+                        io::stdout().flush().unwrap();
+                        let _ib_commit = match git.commit("incremental build", addib) {
                             Ok (oid) => {
                                 println!(" ✓");
                                 oid
                             },
                             Err(err) => panic!(" x\n\t{:?}", err),
                         };
+                        io::stdout().flush().unwrap();
                     }
                 }
         }
