3a110e3f0fb04435770c176259ee8c09ffc20719
RANDRIANAINA Georges Aaron <georges-aaron.randrianaina@irisa.fr>
2022-07-07 17:10:18 +0200

    src/utils: new module for utils functions

---
+++ src/utils.rs
@@ -0,0 +1,69 @@
+
+
+pub fn readconfig(config: &Path) -> HashMap<String, String> {
+
+    let file = match fs::File::open(config) {
+        Ok(file) => file,
+        Err(err) => panic!("Error: {:?} {}", config, err),
+    };
+
+    let mut configuration = HashMap::new();
+
+    for line in BufReader::new(file).lines()
+        .filter(|l| l.is_ok()).map(|l| l.unwrap()) {
+            if line.starts_with('#') {
+                if line.ends_with("is not set") {
+                    let option = line
+                        .strip_prefix("# CONFIG_").unwrap()
+                        .strip_suffix(" is not set").unwrap();
+                    configuration.insert(option.to_string(), "n".to_string());
+                }
+            }else {
+                if !line.is_empty() {
+                    let (option, value) = line.split_once('=').unwrap();
+                    configuration.insert(option.to_string(), value.to_string());
+                }
+            }
+        }
+    configuration
+}
+
+pub fn diffconfig(config1: &Path, config2: &Path)
+                  -> HashMap<String, HashMap<String, String>> {
+
+    let c1 = readconfig(config1);
+    let c2 = readconfig(config2);
+
+    let mut
+        comparison: HashMap<String, HashMap<String, String>> = HashMap::new();
+    comparison.insert("=".to_string(), HashMap::new());
+    comparison.insert("+".to_string(), HashMap::new());
+    comparison.insert("-".to_string(), HashMap::new());
+    comparison.insert("~".to_string(), HashMap::new());
+
+    for (k, v) in c1.iter() {
+        if c2.contains_key(k) {
+            if c1.get(k) == c2.get(k) {
+                comparison.get_mut("=").unwrap()
+                    .insert(k.to_string(), v.to_string());
+            }else {
+                comparison.get_mut("~").unwrap()
+                    .insert(k.to_string(),
+                            format!("{} -> {}", c1.get(k).unwrap().to_string(),
+                                    c2.get(k).unwrap().to_string()));
+            }
+        }else {
+            if c1.get(k) != "n" {
+                comparison.get_mut("-").unwrap()
+                    .insert(k.to_string(), v.to_string());
+            }
+        }
+    }
+    for (k, v) in c2.iter() {
+        if !c1.contains_key(k) && v != "n"{
+            comparison.get_mut("+").unwrap()
+                .insert(k.to_string(), v.to_string());
+        }
+    }
+    comparison
+}
