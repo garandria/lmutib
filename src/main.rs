280493a41b8ced3706db84608e53a02353d613fe
RANDRIANAINA Georges Aaron <georges-aaron.randrianaina@irisa.fr>
2022-08-02 11:17:48 +0200

    src/main: consider base config only for incremental build

--- src/main.rs
+++ src/main.rs
@@ -40,23 +40,28 @@ fn main() {
         .map(|f| f.unwrap().path().to_str().unwrap().to_string()) {
 		    println!("-> {}", metadir);
 		    io::stdout().flush().unwrap();
-		    let mut basename = String::new();
 
-		    for conf in fs::read_dir(Path::new(&metadir)).unwrap()
-		        .filter(|f| f.is_ok()).map(|f| f.unwrap().path().to_str()
-                                           .unwrap().to_string())
-		        .filter(|f| f.ends_with("_base")) {
-		 	        basename = conf.to_string();
-		        }
-		    let baseconf = [configs.to_string(), basename.to_string()]
-                .join("/");
-		    let baseconf_path = Path::new(&basename);
-		    println!("\t~ Base: {}", basename);
-		    io::stdout().flush().unwrap();
-		    println!("\t  Clean build...");
-		    io::stdout().flush().unwrap();
-		    kernel.clean_build(&baseconf_path);
-		    let branch = kernel.config_name_from_path(&baseconf_path);
+            let branch = String::new();
+            if !all_clean {
+		        let mut basename = String::new();
+
+		        for conf in fs::read_dir(Path::new(&metadir)).unwrap()
+		            .filter(|f| f.is_ok()).map(|f| f.unwrap().path().to_str()
+                                               .unwrap().to_string())
+		            .filter(|f| f.ends_with("_base")) {
+		 	            basename = conf.to_string();
+		            }
+		        let baseconf = [configs.to_string(), basename.to_string()]
+                    .join("/");
+		        let baseconf_path = Path::new(&basename);
+		        println!("\t~ Base: {}", basename);
+		        io::stdout().flush().unwrap();
+
+		        println!("\t  Clean build...");
+		        io::stdout().flush().unwrap();
+		        kernel.clean_build(&baseconf_path);
+		        let branch = kernel.config_name_from_path(&baseconf_path);
+            }
 		    for conf in fs::read_dir(Path::new(&metadir)).unwrap()
 		        .filter(|f| f.is_ok()).map(|f| f.unwrap().path().to_str()
                                            .unwrap().to_string())
